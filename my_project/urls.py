66846ca4b1e94335014def4f34db0f944001f88a
Taiwo Muraina <133337460+tmuraina@users.noreply.github.com>
2025-09-21 21:16:26 +0100

    Delete my_project/urls.py

--- my_project/urls.py
+++
@@ -1,6 +0,0 @@
-pythonCopy codefrom django.urls import path
-from .views import BookListCreateAPIView
-
-urlpatterns = [
-    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
-]
