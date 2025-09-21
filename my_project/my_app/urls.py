94027e12fce40efd22e509a8024d32f2a4d985d4
Taiwo Muraina <133337460+tmuraina@users.noreply.github.com>
2025-09-21 21:17:37 +0100

    Create urls.py
    URL patterns for my view

---
+++ my_project/my_app/urls.py
@@ -0,0 +1,6 @@
+pythonCopy codefrom django.urls import path
+from .views import BookListCreateAPIView
+
+urlpatterns = [
+    path("api/books", views.BookListCreateAPIView.as_view(), name="book_list_create"),
+]
